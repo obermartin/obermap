6fa3271fc4757bcd967608003bbb2304f0460560
obermartin <mo@revolverstudios.de>
2026-07-06 16:51:45 +0200

    Housekeeping
    removed leftover utility scripts

--- fix_import.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('frontend/src/components/MapboxMap.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace("import { useAnnotationTools }\nimport { useDisasterAlerts } from '../hooks/useDisasterAlerts'; from '../hooks/useAnnotationTools';", "import { useAnnotationTools } from '../hooks/useAnnotationTools';\nimport { useDisasterAlerts } from '../hooks/useDisasterAlerts';")
-
-with open('frontend/src/components/MapboxMap.tsx', 'w') as f:
-    f.write(content)
--- rewrite_hook.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open('frontend/src/components/MapboxMap.tsx', 'r') as f:
-    mapbox = f.read()
-
-# Since I already deleted the effect_block from MapboxMap.tsx, I need to get it from `git checkout`!
